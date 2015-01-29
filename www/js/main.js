b44afda18501952828898d2e68e6fb5f8d663c88
eashtian <eashtian@EASHTIAN-MOBL1.amr.corp.intel.com>
2015-01-28 16:26:03 -0800

    fix project source files

--- www/js/main.js
+++ www/js/main.js
@@ -1,91 +1,81 @@
-/*jslint sloppy:true, browser:true, devel:true, white:true, vars:true, eqeq:true, nomen:true, unparam:true */
-/*global intel, google, Marker, device */
+/*jslint devel:true, browser:true, unparam:true, white:true */
+/*global admobAd */
 
-var _map = null;
-var _seconds = 30;
-var _llbounds = null;
-var myLatLng;
-var oldLatLng = "";
-var boolTripTrack = true;
-//Create the google Maps and LatLng object 
+/* jshint strict: true, -W097 */
+/* global admobAd, onDocLoad, message */
 
-function drawMap() {
-    //Creates a new google maps object
-    var latlng = new google.maps.LatLng(currentLatitude, currentLongitude);
-    myLatLng = latlng;
-    var mapOptions = {
-        center: latlng,
-        zoom:7,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoomControl: true,
-        zoomControlOptions: {
-            style: google.maps.ZoomControlStyle.SMALL,
-            position: google.maps.ControlPosition.LEFT_TOP
+
+//AdMob Keys
+/*
+    Visit to https://apps.admob.com/ to obtain Ad Unit IDs for displaying Interstitial and Banner ads 
+    Create your App entry and ad unit under the Monetize panel
+*/
+var admob_platform_interstitial_key = 'ADD_YOUR_AD-UNIT-ID_HERE';//Interstitial Ads
+var admob_platform_banner_key = 'ADD_YOUR_AD-UNIT-ID_HERE';//Banner Ads 
+
+/*
+    Function: onReceiveFail
+    Parameter: message - calback message
+    Description: callback fail method
+*/
+function onReceiveFail(message) {
+    'use strict';
+    alert("load fail: " + message.type + "  " + message.data);
+}
+
+/*
+    Function: showInterstitial
+    Parameter: none
+    Description: showInterstitial Ad; executed after init and cache Interstitial
+*/
+function showInterstitial() {
+    'use strict';
+    admobAd.isInterstitialReady(function (isReady) {
+        if (isReady) {
+            admobAd.showInterstitial();
         }
-    };
-    if (boolTripTrack === true) {
-        _map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    });
+}
+
+/*
+Function: onInterstitialReceive
+Parameter: message - callback message
+Description: callback success method
+*/
+function onInterstitialReceive(message) {
+    'use strict';
+    alert("onMInterstitialReceive ,you can show it now");
+    showInterstitial();
+}
+
+/*
+    Function: validateAdMobKey()
+    Parameter: interstitialkey, bannerkey
+    Description: Check if the used keys are not the default text
+*/
+function validateAdMobKey(interstitial_key, banner_key) {
+    'use strict';
+    if ((interstitial_key === 'ADD_YOUR_AD-UNIT-ID_HERE') || (banner_key === 'ADD_YOUR_AD-UNIT-ID_HERE')) {
+        alert("Enter valid Google AdMob* provided Ad Unit IDs in the main.js file!!!");
     }
 }
-//40.7655,-73.97204 = NYC
-var currentLatitude = "40.713768";
-var currentLongitude = "-73.016696";
-var options = {
-    timeout: 10000,
-    maximumAge: 11000,
-    enableHighAccuracy: true
-};
-//Success callback
-var suc = function(p) {
-        console.log("geolocation success", 4);
-        //Draws the map initially
-        if (_map === null) {
-            currentLatitude = p.coords.latitude;
-            currentLongitude = p.coords.longitude;
-            drawMap();
-        } else {
-            myLatLng = new google.maps.LatLng(p.coords.latitude, p.coords.longitude);
-        }
-        //Creates a new google maps marker object for using with the pins
-        if ((myLatLng.toString().localeCompare(oldLatLng.toString())) !== 0) {
-            //Create a new map marker
-            var Marker = new google.maps.Marker({
-                position: myLatLng,
-                map: _map
-            });
-            if (_llbounds === null) {
-                //Create the rectangle in geographical coordinates
-                _llbounds = new google.maps.LatLngBounds(new google.maps.LatLng(p.coords.latitude, p.coords.longitude)); //original
-            } else {
-                //Extends geographical coordinates rectangle to cover current position
-                _llbounds.extend(myLatLng);
-            }
-            //Sets the viewport to contain the given bounds & triggers the "zoom_changed" event
-            _map.fitBounds(_llbounds);
-        }
-        oldLatLng = myLatLng;
-    };
-var fail = function() {
-        console.log("Geolocation failed. \nPlease enable GPS in Settings.", 1);
-    };
-var getLocation = function() {
-        console.log("in getLocation", 4);
-    };
-    //Execute when the DOM loads  
 
-function onDeviceReady() {
-    try {
-        if (navigator.geolocation !== null) {
-            document.getElementById("map_canvas").style.height = screen.height + "px";
-            navigator.geolocation.watchPosition(suc, fail, options);
-        }
-    } catch (e) {
-        alert(e.message);
-    }
-
-    try {
-        //hide splash screen
-        navigator.splashscreen.hide(); 
-    } catch (e) {}
+/*
+    Function: onDocLoad()
+    Parameter: none
+    Description: show the Banner Ad [initBanner(...) then showBanner(...)] or interstitial Ad [initInterstitial(...) then cacheInterstitial() then showInterstitial()]; 
+*/
+function onDocLoad() {
+    'use strict';
+    validateAdMobKey(admob_platform_interstitial_key, admob_platform_interstitial_key);
+    //show Banner ad
+    admobAd.initBanner(admob_platform_banner_key, admobAd.AD_SIZE.BANNER.width, admobAd.AD_SIZE.BANNER.height);//create admob banner
+    admobAd.showBanner(admobAd.AD_POSITION.BOTTOM_CENTER);
+    
+    //show Interstitial ad
+    admobAd.initInterstitial(admob_platform_interstitial_key);//create Interstitial ad
+    document.addEventListener(admobAd.AdEvent.onInterstitialReceive, onInterstitialReceive, false);
+    document.addEventListener(admobAd.AdEvent.onInterstitialFailedReceive, onReceiveFail, false);
+    
+    admobAd.cacheInterstitial();// load admob Interstitial
 }
-document.addEventListener("deviceready", onDeviceReady, false);
